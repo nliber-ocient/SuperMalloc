02772ccb23791a1dd95fd61b832236ba2b9795bd
Nevin Liber <nliber@ocient.com>
2017-10-10 16:59:52 -0500

    Added a test main.cpp

---
+++ allocator/main.cpp
@@ -0,0 +1,22 @@
+#include "supermalloc_allocator.h"
+#include <cool/Out.h>
+#include <cool/pretty_name.h>
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+template<typename Key, typename T, typename Hash = std::hash<Key>, typename Pred = std::equal_to<Key>>
+using SMunordered_map = std::unordered_map<Key, T, Hash, Pred, supermalloc_allocator<std::pair<const Key, T>>>;
+
+int main()
+{
+    SMunordered_map<int, char> mis;
+    mis[2] = '2';
+    mis[3] = '3';
+    mis[5] = '5';
+    mis[7] = '7';
+
+    std::cout << cool::pretty_name(mis) << '\n' << cool::Out(mis) << '\n';
+
+    return 0;
+}
