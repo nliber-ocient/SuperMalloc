0c9254b5ed4cb56030fe4ecc11df3948caeb7bea
Nevin Liber <nliber@ocient.com>
2017-10-11 10:13:08 -0500

    Moved supermalloc_allocator into namespace supermalloc

--- allocator/supermalloc_allocator.h
+++ allocator/supermalloc_allocator.h
@@ -10,61 +10,64 @@
 // // and dump it in namespace pmr for now
 #if __has_include(<memory_resource>)
 #include <memory_resource>
-namespace pmr { using namespace std::pmr; }
+namespace supermalloc::pmr { using namespace std::pmr; }
 #elif __has_include(<experimental/memory_resource>)
 #include <experimental/memory_resource>
-namespace pmr { using namespace std::experimental::pmr; }
+namespace supermalloc::pmr { using namespace std::experimental::pmr; }
 #endif
 
-template<typename T>
-class supermalloc_allocator
+namespace supermalloc
 {
-    template<typename... Ts> constexpr static void maybe_unused(Ts&&...) noexcept {}
-public:
-    using value_type = T;
-    using propagate_on_container_move_assignment = std::true_type;
-    using is_always_equal = std::true_type;
+    template<typename... Ts> constexpr void maybe_unused(Ts&&...) noexcept {}
 
-    constexpr supermalloc_allocator() noexcept = default;
-
-    template<typename U>
-    constexpr supermalloc_allocator(supermalloc_allocator<U> const&) noexcept {}
-
-    T* allocate(size_t n)
+    template<typename T>
+    class supermalloc_allocator
     {
-        size_t alignment = alignof(T);
-        size_t size      = n * sizeof(T);
-        if (T* allocated = static_cast<T*>(supermalloc_aligned_alloc(alignment, size)))
-            return allocated;
+    public:
+        using value_type = T;
+        using propagate_on_container_move_assignment = std::true_type;
+        using is_always_equal = std::true_type;
 
-        throw std::bad_alloc{};
-    }
+        constexpr supermalloc_allocator() noexcept = default;
 
-    void deallocate(T* p, size_t n) noexcept
+        template<typename U>
+        constexpr supermalloc_allocator(supermalloc_allocator<U> const&) noexcept {}
+
+        T* allocate(size_t n)
+        {
+            size_t alignment = alignof(T);
+            size_t size      = n * sizeof(T);
+            if (T* allocated = static_cast<T*>(supermalloc_aligned_alloc(alignment, size)))
+                return allocated;
+
+            throw std::bad_alloc{};
+        }
+
+        void deallocate(T* p, size_t n) noexcept
+        {
+            maybe_unused(n);
+            supermalloc_free(p);
+        }
+    };
+
+    class supermalloc_memory_resource : public pmr::memory_resource
     {
-        maybe_unused(n);
-        supermalloc_free(p);
-    }
-};
+    protected:
+        void* do_allocate(size_t bytes, size_t alignment) override
+        { return supermalloc_aligned_alloc(alignment, bytes); }
 
-class supermalloc_memory_resource : public pmr::memory_resource
-{
-    template<typename... Ts> constexpr static void maybe_unused(Ts&&...) noexcept {}
+        void do_deallocate(void* p, size_t bytes, size_t alignment) override
+        { maybe_unused(bytes, alignment); supermalloc_free(p); }
 
-protected:
-    void* do_allocate(size_t bytes, size_t alignment) override
-    { return supermalloc_aligned_alloc(alignment, bytes); }
+        bool do_is_equal(const memory_resource& other) const noexcept override
+        { maybe_unused(other); return true; }
 
-    void do_deallocate(void* p, size_t bytes, size_t alignment) override
-    { maybe_unused(bytes, alignment); supermalloc_free(p); }
+    public:
+        static supermalloc_memory_resource* mr() noexcept
+        { static supermalloc_memory_resource smr; return &smr; }
+    };
 
-    bool do_is_equal(const memory_resource& other) const noexcept override
-    { maybe_unused(other); return true; }
-
-public:
-    static supermalloc_memory_resource* mr() noexcept
-    { static supermalloc_memory_resource smr; return &smr; }
-};
+} // supermalloc namespace
 
 #endif /* SUPERMALLOC_ALLOCATOR_H_ */
 
